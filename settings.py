ce2313537d8a70609719f9e9a35b8274b29822fb
Bryan Wyatt <brwyatt@gmail.com>
2016-02-20 14:06:00 -0800

    Add config file reader

---
+++ settings.py
@@ -0,0 +1,34 @@
+import json
+import os
+
+from logging import getLogger
+log = getLogger('gunicorn.error')
+
+class Settings:
+    sysConfigFile = '/etc/authservicesapi.conf'
+    loaded = False
+
+    config = {
+            'cassandra': {
+                'nodes': [ '127.0.0.1' ],
+                'port': '9042'
+                }
+            }
+
+    def getConfig():
+        if not Settings.loaded:
+            if os.path.isfile(Settings.sysConfigFile):
+                with open(Settings.sysConfigFile, 'r') as f:
+
+                    def mergeConfig(a, b):
+                        for key in a.keys():
+                            if key in b:
+                                if isinstance(a[key],dict):
+                                    a[key] = mergeConfig(a[key],b[key])
+                                else:
+                                    a[key] = b[key]
+                        return a
+
+                    Settings.config = mergeConfig(Settings.config,json.load(f))
+            Settings.loaded = True
+        return Settings.config
