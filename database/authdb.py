2024333e5f2872164314b695557353a814cd7e36
Bryan Wyatt <brwyatt@gmail.com>
2016-03-12 11:53:23 -0800

    Add documentation for AuthDB class

--- database/authdb.py
+++ database/authdb.py
@@ -4,10 +4,21 @@ from functools import wraps
 
 
 class AuthDB:
+    """
+    Class with static methods for interacting with the AuthDB using a singleton
+    CassandraCluster Session object.
+    """
+
     keyspace = 'authdb'
     session = None
 
     def sessionQuery(keyspace):
+        """
+        Wrapper to ensure session creation for each query
+
+        :keyspace:
+            Keyspace to use for session creation.
+        """
         def sessionQueryWrapper(func):
             @wraps(func)
             def func_wrapper(*args, **kwargs):
@@ -20,6 +31,20 @@ class AuthDB:
     @sessionQuery(keyspace)
     def createUser(org, username, email, parentuser,
                    consistency=ConsistencyLevel.LOCAL_QUORUM):
+        """
+        Create a user in the authdb.users table.
+
+        :org:
+            Name of organization
+        :username:
+            Name of user
+        :email:
+            Email address for the user
+        :parentuser:
+            Parent user for this user (in the form of user@org) or None
+        :consistency:
+            Cassandra ConsistencyLevel (default LOCAL_QUORUM)
+        """
         createUserQuery = CassandraCluster.getPreparedStatement(
             """
             INSERT INTO users ( org, username, email, parentuser, createdate )
@@ -31,6 +56,15 @@ class AuthDB:
 
     @sessionQuery(keyspace)
     def getOrgSetting(org, setting):
+        """
+        Get a setting/property for an organization from the authdb.orgsettings
+        table.
+
+        :org:
+            Name of organization
+        :setting:
+            Setting/property name
+        """
         checkOrgSetting = CassandraCluster.getPreparedStatement(
             """
             SELECT value FROM orgsettings
@@ -41,6 +75,14 @@ class AuthDB:
 
     @sessionQuery(keyspace)
     def getUser(org, username):
+        """
+        Retrieve a user from the authdb.users table
+
+        :org:
+            Name of organization the user belongs to
+        :username:
+            Name of the user
+        """
         getUserQuery = CassandraCluster.getPreparedStatement(
             """
             SELECT username, org, parentuser, createdate FROM users
@@ -50,4 +92,13 @@ class AuthDB:
         return AuthDB.session.execute(getUserQuery, (org, username))
 
     def userExists(org, username):
+        """
+        Check if a user exists in authdb.users table. Uses getUser() for user
+        lookup.
+
+        :org:
+            Name of organization for user
+        :username:
+            Name of the user
+        """
         return len(AuthDB.getUser(org, username).current_rows) > 0
